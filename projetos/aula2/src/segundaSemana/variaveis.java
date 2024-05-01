15634d102ef9508357822808f5fe8a9ec9aee9f4
Your Name <you@example.com>
2024-05-01 16:29:49 -0300

    meus projetos java

---
+++ projetos/aula2/src/segundaSemana/variaveis.java
@@ -0,0 +1,18 @@
+package segundaSemana;
+
+public class variaveis {
+    public static void main(String[] args) throws Exception
+    {
+     int varlo=88;
+     int valor2=9;
+     System.out.println("o primeiro valor =:"+varlo);
+     System.out.println("o segundo valor =:"+valor2);
+     System.out.println("Basico da matematica com os valor \n\n");
+     System.out.println("Soma=:"+(varlo+valor2));
+     System.out.println("Menos=:"+(varlo-valor2));
+     System.out.println("Multiplicao =:"+(varlo*valor2));
+     System.out.println("Divi =:"+((float)varlo/valor2));
+      
+      
+}
+}
