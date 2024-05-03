e281b626ff4aa949e1fc93b18a3a04804ead6f5c
Your Name <you@example.com>
2024-05-03 01:29:20 -0300

    Projeto de terminal

---
+++ Simulando Uma Conta Bancária/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal
+ {
+    public static void main(String[] args) throws Exception
+     {
+        Cadastrar cadastro =new Cadastrar();
+
+        cadastro.castroCliente(); 
+        cadastro.MostrarCadastroCliente();  
+    
+       
+    }
+
+
+}
